373d40a3646a58f43df6947b1aa3f735fa613164
shau-git <k.shaujie@gmail.com>
2026-01-31 22:15:47 +0800

    final deployment fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
+import { PrismaPlugin } from '@prisma/nextjs-monorepo-workaround-plugin';
 const nextConfig = {
   /* config options here */
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.plugins = [...(config.plugins || []), new PrismaPlugin()];
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
