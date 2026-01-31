6ac3d94e890daa887c125944de0ae4f818d98683
shau-git <k.shaujie@gmail.com>
2026-01-31 22:59:59 +0800

    message

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,7 @@
 /** @type {import('next').NextConfig} */
-import { PrismaPlugin } from '@prisma/nextjs-monorepo-workaround-plugin';
 const nextConfig = {
   /* config options here */
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      config.plugins = [...(config.plugins || []), new PrismaPlugin()];
-    }
-    return config;
-  },
+  turbo: false,
 };
 
 export default nextConfig;
